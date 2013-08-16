4944b78755ae5a684f0a2fdff3f6028d24bc3189
Mischa Taylor <mischa@misheska.com>
2013-08-16 00:35:11 -0700

    Initial cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# wheezy-lamp cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'wheezy-lamp'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures wheezy-lamp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
